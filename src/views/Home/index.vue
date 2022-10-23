6c7ff01710e984be7512117f134fef33cec1ee45
xiaobai <1249448161@qq.com>
2022-10-23 19:57:49 +0800

    0.0.1

---
+++ src/views/Home/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+	<form > 
+		<input type="text">
+		<input type="password">
+		<button>登陆</button>
+	</form>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
